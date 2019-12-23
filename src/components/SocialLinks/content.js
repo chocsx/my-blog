68e94bb5d33f02ad9e212b5cc9506f07b2a0d508
Mateus Sobrinho <mateussobrinho@vhsolucoes.com.br>
2019-12-23 02:33:07 -0300

    add sidebar to project

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/chocsx",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/chocsx",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/chocsx",
+  },
+  {
+    label: "Youtube",
+    url: "https://youtube.com/howtophp",
+  },
+]
+
+export default links
