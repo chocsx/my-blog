08c52195f67e959ed0630208faca3340cd5caf24
Mateus Sobrinho <mateus.sobrinho@fcamara.com.br>
2020-06-26 14:56:36 -0300

    Fix pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import propTypes from "prop-types"
 import { Link } from "gatsby"
 
 import * as S from "./styled"
@@ -10,12 +11,21 @@ const Pagination = ({
   numPages,
   prevPage,
   nextPage,
-}) => {
+}) => (
   <S.PaginationWrapper>
     {!isFirst && <Link to={prevPage}>página anterior</Link>}
-    {currentPage} de {numPages}
+    <p>
+      {currentPage} de {numPages}
+    </p>
     {!isLast && <Link to={nextPage}>próxima página </Link>}
   </S.PaginationWrapper>
+)
+Pagination.prototype = {
+  isFirst: propTypes.bool.isRequired,
+  isLast: propTypes.bool.isRequired,
+  currentPage: propTypes.number.isRequired,
+  numPages: propTypes.number.isRequired,
+  prevPage: propTypes.string,
+  nextPage: propTypes.string,
 }
-
 export default Pagination
