013394a1db73401e9d6c616539c7862d9529d0ec
Mateus Sobrinho <mateus.sobrinho@fcamara.com.br>
2020-08-30 07:45:05 -0300

    Add page transition with AniLink

--- src/components/MenuLinks/index.js
+++ src/components/MenuLinks/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import getThemeColor from "../../utils/getThemeColor"
+
 import links from "./content"
 import * as S from "./styled"
 
@@ -8,7 +10,14 @@ const MenuLinks = () => (
     <S.MenuLinksList>
       {links.map((link, i) => (
         <S.MenuLinksItem key={i}>
-          <S.MenuLinksLink to={link.url} activeClassName="active">
+          <S.MenuLinksLink
+            cover
+            direction="left"
+            bg={getThemeColor()}
+            duration={0.6}
+            to={link.url}
+            activeClassName="active"
+          >
             {link.label}
           </S.MenuLinksLink>
         </S.MenuLinksItem>
