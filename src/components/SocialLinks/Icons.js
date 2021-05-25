41bcfa2ddb3d0188c154529deec442b4ca35c066
Mateus Sobrinho <mateus.webphp@gmail.com>
2021-05-25 06:20:39 -0300

    fix(update): all dependencies

--- src/components/SocialLinks/Icons.js
+++ src/components/SocialLinks/Icons.js
@@ -1,7 +1,7 @@
-import { Github } from "styled-icons/boxicons-logos/Github"
-import { Twitter } from "styled-icons/boxicons-logos/Twitter"
-import { Youtube } from "styled-icons/boxicons-logos/Youtube"
-import { Instagram } from "styled-icons/boxicons-logos/Instagram"
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { Youtube } from "@styled-icons/boxicons-logos/Youtube"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
 
 const Icons = {
   Github,
