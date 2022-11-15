20e6b8b7ec44f590bfe2ed4e778fbbb7cd5890bb
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-11-15 17:34:31 -0300

    add folders

---
+++ src/content/blog/2015-01-03-two-things-are-infinite.md
@@ -0,0 +1,19 @@
+---
+date: 2015-01-03 05:54:23
+title: Two things are infinite
+description: and I'm not sure about the universe.
+category: Misc
+background: "#7AAB13"
+---
+
+# Lorem Ipsum
+
+Teste de parágrafo com markdown pesquisando no [Google](https://google.com)
+
+
+```javascript
+a=3
+puts a
+```
+
+
---
+++ src/pages/[slug].js
@@ -0,0 +1,33 @@
+import { remark } from 'remark'
+import html from 'remark-html'
+import { getPostBySlug, getAllPosts } from '../utils/posts'
+
+export async function getStaticProps({ params }) {
+  const post = getPostBySlug(params.slug)
+  const markdown = await remark()
+    .use(html)
+    .process(post.content || '')
+  const content = markdown.toString()
+
+  return {
+    props: {
+      ...post,
+      content,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts()
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
